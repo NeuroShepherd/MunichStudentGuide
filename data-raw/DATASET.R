4edbc360e537891bad352997dde14fb4510f5c5b
Patrick Callahan <patricktcallahan18@gmail.com>
2023-09-01 13:04:39 +0200

    Update parameters data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,7 +7,10 @@ global_parameters <- list(
   funding_year = "2023",
   tax_threshold = "€520",
   min_wage_date = "October 1st, 2022",
-  min_wage_amount = 12
+  min_wage_amount = 12,
+  wise_referral_link = "https://wise.com/invite/dic/patrickc2551",
+  coracle_referral_link = "",
+  n26_referral_link = "https://n26.com/r/patrickc4192"
 )
 
 usethis::use_data(global_parameters, overwrite = TRUE)
